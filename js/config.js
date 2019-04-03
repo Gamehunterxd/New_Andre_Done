6a53a6b8499915f902ba60d2c19bedaa15561818
Montaser Zaghmout <mont0004@edu.easj.dk>
2019-04-03 12:44:54 +0200

    rettelser

--- js/config.js
+++ js/config.js
@@ -4,13 +4,13 @@ $(function() {
 	    // array representing the format and columns of the cart, see
 	    // the cart columns documentation
 	    cartColumns: [
-	        { attr: "name" , label: "Name" },
-	        { attr: "price" , label: "Price", view: 'currency' },
+	        { attr: "name" , label: "Navn" },
+	        { attr: "price" , label: "Pris", view: 'currency' },
 	        { view: "decrement" , label: false },
-	        { attr: "quantity" , label: "Qty" },
+	        { attr: "quantity" , label: "Stk" },
 	        { view: "increment" , label: false },
 	        { attr: "total" , label: "SubTotal", view: 'currency' },
-	        { view: "remove" , text: "Remove" , label: false }
+	        { view: "remove" , text: "Fjern" , label: false }
 	    ],
 
 	    // "div" or "table" - builds the cart as a table or collection of divs
@@ -19,18 +19,18 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "you@yours.com"
+	        email: "Mont0004@edu.easj.dk"
 	    },
 
 	    // set the currency, see the currency reference for more info
-	    currency: "USD",
+	    currency: "DKK",
 
 	    // collection of arbitrary data you may want to store with the cart,
 	    // such as customer info
 	    data: {},
 
 	    // set the cart langauge (may be used for checkout)
-	    language: "english-us",
+	    language: "Danish-da",
 
 	    // array of item fields that will not be sent to checkout
 	    excludeFromCheckout: [
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "https://upload.wikimedia.org/wikipedia/commons/a/af/Newandre.png",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
